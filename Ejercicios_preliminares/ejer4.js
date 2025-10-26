b34c899b252149cb7e29a319851fd2889a6cd248
mBerasategui-ehu <mberasategui022@ikasle.ehu.eus>
2025-10-26 09:27:08 +0100

    2.1.1-Ejercicios

---
+++ Ejercicios_preliminares/ejer4.js
@@ -0,0 +1,10 @@
+fetch('http://api.football-data.org/v4/competitions')
+    .then(
+        r => r.json())
+    .then(
+        data => {
+            let grandes_ligas = data.competitions.filter(comp => comp.plan === 'TIER_ONE')
+            let cuatro_grandes_ligas = grandes_ligas.filter(comp => (comp.area.code === 'ESP' || comp.area.code === 'ENG' || comp.area.code === 'ITA' || comp.area.code === 'FRA') && (comp.name !== 'Championship'))
+            console.log(cuatro_grandes_ligas)
+        }
+    )
