a0815934fd8a635e90868e41797d32055c777a8f
mBerasategui-ehu <mberasategui022@ikasle.ehu.eus>
2025-12-28 17:46:31 +0100

    Arreglar error register

--- src/models/User.js
+++ src/models/User.js
@@ -31,16 +31,12 @@ const UserSchema = new mongoose.Schema({
 
 });
 
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-        //Usamos 10 rondas para generar el salt
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    // Usamos 10 rondas para generar el salt
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.comparePassword = async function(candidatePassword) {
